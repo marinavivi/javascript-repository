8a98d89d34d7a47e28d5a49d7ed47c8ad1e3cde1
Admin <admin@Nikolas-MacBook-Pro.local>
2022-05-05 12:27:50 +0200

    js anonymous functions #CSC3-7

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
-console.log('radi')
+const numbers = [2, 4, 6, 8];
+
+
+let squareNumbers = numbers.map((number) => {
+    const result = Math.pow(number, 2);
+
+    return result;
+})
+console.log('square: ' + squareNumbers);
+
+
+(function (num_A, num_B) 
+{
+    let num_C = num_A + num_B;
+
+    console.log('summ: ' + num_C);
+}
+)(10, 20);
+
+
+const timesTwo = (number) => {
+
+    return number * 2;
+}
+
+console.log('result:' + timesTwo(10));
